e60235bb6ae0305b9c50d2636797b2a2c80bee6e
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-05-27 23:26:35 +0530

    Implemented vowel checking program using conditional statements in C

---
+++ C-Practice-Questions/CheckVowel.c
@@ -0,0 +1,42 @@
+//Accept an character from user  and check whether that character is vowel (a,e,i,o,u) or not.
+// Input: E           output: TRUE
+// Input: d           output: FALSE
+
+#include<stdio.h>
+
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkVowel( char cValue)
+{
+    if( (cValue == 'A') || (cValue == 'E') || (cValue == 'I') || (cValue == 'O') || (cValue == 'U') ||
+        (cValue == 'a') || (cValue == 'e') || (cValue == 'i') || (cValue == 'o') || (cValue == 'u'))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter character\n");
+    scanf("%c", &cValue);
+
+    bRet = ChkVowel(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is Vowel");
+    }
+    else
+    {
+        printf("It is not a Vowel");
+    }
+    return 0;
+}
