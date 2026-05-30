d4c4fde05cc0d7ea41a60cd62effc8eac07421c3
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-05-30 23:07:08 +0530

    Implemented logic to count even digits in a number

---
+++ C-Practice-Questions/EvenDigitCount.c
@@ -0,0 +1,48 @@
+/*
+Write a program which accepts number from user an return the count of even
+Input : 2395
+Output : 1
+Input : 1018
+Output : 2
+Input : -1018
+Output : 2
+Input : 8462
+Output : 4
+*/
+
+#include <stdio.h>
+
+int CountEven(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if((iDigit % 2) == 0)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d", &iValue);
+
+    iRet = CountEven(iValue);
+    printf( "%d\n", iRet);
+
+    return 0;
+}
