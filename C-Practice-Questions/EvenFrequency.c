77cf1e144240ae18c3b33a9efd45677731652d17
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-06-06 21:20:37 +0530

    Implemented frequency count of even elements in array

---
+++ C-Practice-Questions/EvenFrequency.c
@@ -0,0 +1,54 @@
+/*
+    Accept N from user and return frequency of even numbers.
+    Input : N : 6
+            Elements : 85 66 3 80 93 88
+    Output : 3
+*/
+
+#include <stdio.h>
+
+int CountEven(int Arr[], int iLength)
+{
+    int iCnt = 0, iEvenCount = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] % 2 == 0)
+        {
+            iEvenCount++;
+        }
+    }
+
+    return iEvenCount;
+}
+
+int main()
+{
+    int iSize = 0, iCnt = 0, iRet = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : \n");
+    scanf("%d", &iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter %d elements : \n", iSize);
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = CountEven(p, iSize);
+
+    printf("Frequency of even numbers is : %d\n", iRet);
+
+    free(p);
+
+    return 0;
+}
