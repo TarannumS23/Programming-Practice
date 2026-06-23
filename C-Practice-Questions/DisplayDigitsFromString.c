4f419ef25624994cf67154b46acad4bda0f325d0
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-06-23 16:13:21 +0530

    Extract and display digits from a string

---
+++ C-Practice-Questions/DisplayDigitsFromString.c
@@ -0,0 +1,32 @@
+/*
+    Write a program which accepts string from user and display only digits from that string.
+    Input : "Marve89llous121"
+    Output : 89121
+    Input : "Demo"
+    Output : 
+*/
+
+#include<stdio.h>
+
+void DisplayDigit(char *str)
+{
+    while(*str != '\0')
+    {
+        if(*str >= '0' && *str <= '9')
+        {
+            printf("%c",*str);
+        }
+        str++;
+    }
+}
+int main()
+{
+    char arr[20] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",arr);
+
+    DisplayDigit(arr);
+
+    return 0;
+}
