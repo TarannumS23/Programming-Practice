fdaae8850251bde5332873778ecf064f2d61df00
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-06-24 20:31:21 +0530

    Copy uppercase characters into a new string

---
+++ C-Practice-Questions/CopyCapitalCharacters.c
@@ -0,0 +1,34 @@
+/*
+    Write a program which accept string from user and copy capital characters of that string into another string.
+    (Implement strcpy() function).
+    Input : "Marvellous Multi OS"
+    Output : "MMOS"        
+*/
+
+#include<stdio.h>
+
+void StrCpyCap(char *src, char *dest)
+{
+    while((*src != '\0'))
+    {
+        if((*src >= 'A') && (*src <= 'Z'))
+        {
+            *dest = *src;
+             dest++;
+        }       
+        src++;
+    }
+    *dest = '\0';
+}
+
+int main()
+{
+    char arr[30] = "Marvellous Multi OS";
+    char brr[30];     // Empty string
+
+    StrCpyCap(arr,brr);
+
+    printf("%s",brr);      // MMOS
+
+    return 0;
+}
