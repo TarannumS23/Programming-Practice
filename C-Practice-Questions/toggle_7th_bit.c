dd9fb0fd4e42e5f1402dc936a3f1abc333104264
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-07-01 20:42:36 +0530

    Implement program to toggle the 7th bit of a number

---
+++ C-Practice-Questions/toggle_7th_bit.c
@@ -0,0 +1,31 @@
+/*
+    Write a program which accept one number from user and toggle
+    7th bit of that number. Return modified number.
+    Input : 137
+    Output : 201
+*/
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT ToggleBit(UINT iNo)
+{
+    UINT iMask = (1 << 6);
+
+    return (iNo ^ iMask);
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%u",&iValue);
+
+    iRet = ToggleBit(iValue);
+
+    printf("Modified number is : %u\n",iRet);
+
+    return 0;
+}
