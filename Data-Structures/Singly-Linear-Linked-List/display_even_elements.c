0bae9bc4615854e09e6d088409024118117d72c8
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-07-09 15:37:04 +0530

    Implemented display of even elements from singly linear linked list

---
+++ Data-Structures/Singly-Linear-Linked-List/display_even_elements.c
@@ -0,0 +1,84 @@
+/*
+    Display Only Even Elements
+    Print only even elements.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void DisplayEven(PNODE first)
+{
+    while(first != NULL)
+    {
+        if(first->data % 2 == 0)
+        {
+            printf(" %d ", first->data);
+        }
+        first = first->next;      
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first->next;
+    }
+
+    printf("NULL\n");
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp -> next = newn;
+    }
+
+}
+
+int main()
+{
+    PNODE head = NULL;
+    
+    InsertLast(&head,11);
+    InsertLast(&head,22);
+    InsertLast(&head,51);
+    InsertLast(&head,102);
+
+    Display(head);
+
+   printf("Even elements are : ");
+   DisplayEven(head);
+
+    return 0;
+}
