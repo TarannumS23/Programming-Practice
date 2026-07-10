b1fb1110afcc7a5990da1f960ebdfae9255a3fdc
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-07-10 13:36:51 +0530

    Implemented counting of elements greater than specified value in singly linear linked list

---
+++ Data-Structures/Singly-Linear-Linked-List/count_elements_greater_than_x.c
@@ -0,0 +1,89 @@
+/*
+    Count Numbers Greater Than X
+    count elements greater than given number.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+int CountGreater(PNODE first, int X)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        if(first->data > X)
+        {
+            iCount++;
+        }
+        first = first->next;      
+    }
+    return iCount;
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first->next;
+    }
+
+    printf("NULL\n");
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp -> next = newn;
+    }
+
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet =  0;
+    
+    InsertLast(&head,11);
+    InsertLast(&head,22);
+    InsertLast(&head,51);
+    InsertLast(&head,102);
+
+    Display(head);
+
+    iRet = CountGreater(head,50);
+
+    printf("Count of elements greater than given number is : %d\n",iRet);
+
+    return 0;
+}
