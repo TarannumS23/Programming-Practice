4dbde252810d0a11d5130cf195732e8b15c8d0c6
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-07-22 23:18:56 +0530

    Implemented generic program to find the maximum element using function templates

---
+++ Generic-Programming-Questions/generic_maximum_element.cpp
@@ -0,0 +1,34 @@
+// Write generic program to accept N values from user and return largest values.
+
+#include<iostream>
+using namespace std;
+
+template<class T>
+T Max(T *arr, int iSize)
+{
+    T Max = arr[0];
+
+    for(int i = 1; i < iSize; i++)
+    {
+        if(arr[i] > Max)
+        {
+            Max = arr[i];
+        }
+    }
+
+    return Max;
+}
+
+int main()
+{
+    int arr[] = {10,20,30,40,50};
+    float brr[] = {10.0f,3.7f,9.8f,8.7f};
+
+    int iRet = Max(arr,5);
+    cout<<"Maximum integer : "<<iRet<<"\n";
+
+    float fRet = Max(brr,4);
+    cout<<"Maximum float : "<<fRet<<"\n";
+
+    return 0;
+}
