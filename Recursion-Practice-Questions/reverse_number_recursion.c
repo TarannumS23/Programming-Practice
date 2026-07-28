f95db31083292083ab575dea1936a1e6c1cfb240
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-07-28 16:58:56 +0530

    Implement recursive program to reverse digits of a number

---
+++ Recursion-Practice-Questions/reverse_number_recursion.c
@@ -0,0 +1,39 @@
+/*
+    Write a recursive program which accept number from user and return its reverse number.
+
+    Input :  523
+    Output : 325
+*/
+
+#include<stdio.h>
+
+int Reverse(int iNo)
+{
+    int iDigit = 0;
+    static int iRev = 0;
+
+    if(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iRev = (iRev * 10) + iDigit;
+
+        Reverse(iNo / 10);
+    }
+
+    return iRev;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = Reverse(iValue);
+
+    printf("Reversed number is : %d\n",iRet);
+
+    return 0;
+}
