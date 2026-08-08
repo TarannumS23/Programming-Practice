9fff0725c9a0f2dcb36955789fde190c6fe7b68c
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-08-08 17:21:22 +0530

    Merge file names and contents into Marvellous.txt

---
+++ File-Handling-Practice-Questions/Java-Programming/MergeFilesWithNames.java
@@ -0,0 +1,61 @@
+/*
+    Write Java program to accept directory name from user and write
+    file name followed by its contents into Marvellous.txt file.
+*/
+
+import java.io.*;
+import java.util.*;
+
+class FileX
+{
+    public static void MergeFiles(String DirectoryName) throws IOException
+    {
+        File fobjfolder = new File(DirectoryName);
+
+        File fArr[] = fobjfolder.listFiles();
+
+        FileOutputStream foobj = new FileOutputStream("Marvellous.txt");
+
+        for(int i = 0; i < fArr.length; i++)
+        {
+            if(fArr[i].isFile())
+            {
+                String FileName = fArr[i].getName();
+
+                foobj.write(FileName.getBytes());
+                foobj.write('\n');
+
+                FileInputStream fiobj = new FileInputStream(fArr[i]);
+
+                byte Buffer[] = new byte[1024];
+                int iRet = 0;
+
+                while((iRet = fiobj.read(Buffer)) != -1)
+                {
+                    foobj.write(Buffer, 0, iRet);
+                }
+
+                foobj.write('\n');
+
+                fiobj.close();
+            }
+        }
+
+        foobj.close();
+    }
+}
+
+class MergeFilesWithNames
+{
+    public static void main(String A[]) throws IOException
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        String DirectoryName = null;
+
+        System.out.println("Enter Directory Name : ");
+        DirectoryName = sobj.nextLine();
+
+        FileX.MergeFiles(DirectoryName);
+    }
+}
